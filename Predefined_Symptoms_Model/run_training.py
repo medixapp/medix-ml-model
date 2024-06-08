bc715470696637ad9dbcf498fb2d4561b281d927
WilliamDevin23 <wdevinsp@gmail.com>
2024-06-08 11:37:16 +0700

    Use variable to write all symptoms and classes

--- Predefined_Symptoms_Model/run_training.py
+++ Predefined_Symptoms_Model/run_training.py
@@ -12,7 +12,7 @@ def run_training():
     df_sampled = sample_multiple_n(df, "Gejala", "Penyakit", num_samples=20)
 
     # Get one hot encoded data
-    df_final = one_hot_encode_symptoms(df_sampled, 'Gejala', 'Penyakit')
+    df_final, all_symptoms = one_hot_encode_symptoms(df_sampled, 'Gejala', 'Penyakit')
 
     # Get training and testing set
     df_train, df_test = shuffle_and_split(df_final, 'Penyakit', test_size=0.2)
@@ -33,22 +33,19 @@ def run_training():
     model = compile_model(model)
 
     # Train the model
-    history = train_model(model, x_train, y_train_encoded, x_test, y_test_encoded)
+    _ = train_model(model, x_train, y_train_encoded, x_test, y_test_encoded)
 
     # Evaluate the model
     loss, accuracy = model.evaluate(x_test, y_test_encoded)
-    print(f'Test accuracy: {accuracy}')
+    print(f'Test accuracy: {accuracy:.2f}')
+    print(f'Test loss: {loss:.2f}')
 
     save_model(model)
 
-    unique_symptoms = set()
-    for symptoms in df['Gejala'].str.split(', '):
-        unique_symptoms.update(symptoms)
     with open('all_symptoms.txt', 'w') as txt_file:
-        txt_file.write(','.join(sorted(unique_symptoms)))
+        txt_file.write(",".join(all_symptoms))
 
-    unique_labels = df['Penyakit'].unique().tolist()
-    class_dict = {i: label for i, label in enumerate(unique_labels)}
+    class_dict = {i: label_encoder.inverse_transform([i])[0] for i in range(14)}
     with open('class_dict.json', 'w') as json_file:
         json.dump(class_dict, json_file)
 
